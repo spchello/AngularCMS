f848bb78621a4b1e416874a7e76e4af357b7fa5f
shih <peijyun.shih@gmail.com>
2016-06-06 10:43:08 +0800

    ADD CustomerService.js 與WebAPI溝通的程式

---
+++ CMS.Web/App/CustomerApp/CustomerService.js
@@ -0,0 +1,12 @@
+﻿MainApp.factory("CustService", function($http, $q){
+    return {
+        //分頁
+        getData: function (currentPage, pageSize) {
+            var deferred = $q.defer();
+            $http.get('/api/Customer', { params: { CurrPage: currentPage, PageSize: pageSize } })
+            .success(deferred.resolve)
+            .error(deferred.reject);
+            return deferred.promise;
+        }
+    }
+});
