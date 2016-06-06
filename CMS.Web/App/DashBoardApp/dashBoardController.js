d84ae27683e7c48fb951ef7e438a92643f29dcb1
spj <peijyun.shih@gmail.com>
2016-06-06 13:50:23 +0800

    ADD Controller命名為"DashBoardCtrl"，向Service要資料，並給到$scope裡的Datas

---
+++ CMS.Web/App/DashBoardApp/dashBoardController.js
@@ -0,0 +1,15 @@
+﻿MainApp.controller('DashBoardCtrl', function ($scope, $location, $route, DashBoardService) {
+
+    $scope.IsLoad = true;
+
+    DashBoardService.getData()
+        .then(function (data) {
+            //success
+            $scope.datas = data;
+            $scope.IsLoad = false;
+        }, function (data) {
+            //error
+            $scope.error = "取得資料錯誤!";
+            $scope.IsLoad = false;
+        });
+});
