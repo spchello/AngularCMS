8a0d72de3fc460e5850c4291fd2d021dade5eeb3
shih <peijyun.shih@gmail.com>
2016-06-06 11:34:23 +0800

    ADD  新增getCustomer. AddData. Update. deleteCustomer方法

--- CMS.Web/App/CustomerApp/CustomerService.js
+++ CMS.Web/App/CustomerApp/CustomerService.js
@@ -7,6 +7,38 @@
             .success(deferred.resolve)
             .error(deferred.reject);
             return deferred.promise;
+        },
+        //取得單筆
+        getCustomer: function (CustomerID) {
+            var deferred = $q.defer();
+            $http.get('/api/Customer', { params: { CustomerID: CustomerID } })
+            .success(deferred.resolve)
+            .error(deferred.reject);
+            return deferred.promise;
+        },
+        //新增單筆
+        AddData: function (Customer) {
+            var deferred = $q.defer();
+            $http.post('/api/Customer', Customer)
+            .success(deferred.resolve)
+            .error(deferred.reject);
+            return deferred.promise;
+        },
+        //更新單筆
+        Update: function (Customer) {
+            var deferred = $q.defer();
+            $http.put('/api/Customer', Customer)
+            .success(deferred.resolve)
+            .error(deferred.reject);
+            return deferred.promise;
+        },
+        //刪除單筆
+        deleteCustomer: function (Customer) {
+            var deferred = $q.defer();
+            $http.delete('/api/Customer?id=' + Customer.CustomerID)
+            .success(deferred.resolve)
+            .error(deferred.reject);
+            return deferred.promise;
         }
     }
 });
