aa1aefcdebfaf2bfeaf46fc097058044d22394b7
shih <peijyun.shih@gmail.com>
2016-06-06 11:30:14 +0800

    ADD  POST(新增) .PUT(修改) . Delete(刪除),  與BLL溝通和錯誤例外處理

--- CMS.Web/API/CustomerController.cs
+++ CMS.Web/API/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using CMS.BLL.Services;
+using CMS.Domain.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -49,6 +50,52 @@ namespace CMS.Web.API
                 //回傳一個JSON Object
                 var Rvl = new { Total = TotalRaw, Data = datas };
                 return Request.CreateResponse(HttpStatusCode.OK, Rvl);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message.ToString());
+            }
+        }
+
+        //新增單筆
+        // POST: api/Customer
+        public HttpResponseMessage Post(CustomerViewModel models)
+        {
+            try
+            {
+                service.AddCustomer(models);
+                return Request.CreateResponse();
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message.ToString());
+            }
+        }
+
+        //更新單筆
+        // PUT: api/Customer/5
+        public HttpResponseMessage Put(CustomerViewModel models)
+        {
+            try
+            {
+                service.SaveCustomer(models);
+                return Request.CreateResponse();
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message.ToString());
+            }
+        }
+
+
+        //刪除單筆
+        // DELETE: api/Customer/5
+        public HttpResponseMessage Delete(string id)
+        {
+            try
+            {
+                service.Delete(id);
+                return Request.CreateResponse();
             }
             catch (Exception ex)
             {
