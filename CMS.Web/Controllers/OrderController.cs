47bbae6537b1bddeca21bd044f6fa2a567b0c7df
spj <peijyun.shih@gmail.com>
2016-06-06 15:34:24 +0800

    ADD Export方法: 顯示資料清單，並透過.rdlc匯出成Word.PDF.Excel。

--- CMS.Web/Controllers/OrderController.cs
+++ CMS.Web/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using CMS.BLL.Services;
+using Microsoft.Reporting.WebForms;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +16,72 @@ namespace CMS.Web.Controllers
         {
             var models = OrderSevice.GetCustomerOrders();
             return View(models);
+        }
+
+        public ActionResult Export(string type)
+        {
+            // 讀取.rdlc檔
+            LocalReport lr = new LocalReport();
+            string path = Path.Combine(Server.MapPath("~/Rdlc"), "rdlcOrders.rdlc");
+            lr.ReportPath = path;
+
+            //取得訂單資料
+            var list = OrderSevice.GetCustomerOrders();
+
+            //設定資料庫
+            ReportDataSource rd = new ReportDataSource("DataSet1", list);
+            lr.DataSources.Add(rd);
+
+            //檔案類型 type
+            string reportType = type;
+            string mineType;
+            string encoding;
+            string fileNameExtension;
+            string deviceInfo =
+             "<DeviceInfo>" +
+            "  <OutputFormat>" + type + "</OutputFormat>" +
+            "  <PageWidth>8.5in</PageWidth>" +
+            "  <PageHeight>11in</PageHeight>" +
+            "  <MarginTop>0.5in</MarginTop>" +
+            "  <MarginLeft>1in</MarginLeft>" +
+            "  <MarginRight>1in</MarginRight>" +
+            "  <MarginBottom>0.5in</MarginBottom>" +
+            "</DeviceInfo>";
+
+            Warning[] warings;
+            string[] streams;
+            byte[] renderedBytes;
+
+            renderedBytes = lr.Render(
+                reportType,
+                deviceInfo,
+                out mineType,
+                out encoding,
+                out fileNameExtension,
+                out streams,
+                out warings
+                );
+
+            string fileType;
+            switch (type)
+            {
+                case "Excel":
+                    fileType = "xls";
+                    break;
+
+                case "word":
+                    fileType = "doc";
+                    break;
+                case "pdf":
+                    fileType = "pdf";
+                    break;
+                default:
+                    fileType = "";
+                    break;
+            }
+
+            // 回傳檔案 (Excel xls/ pdf pdf/word doc)
+            return File(renderedBytes, mineType, "Report." + fileType);
         }
     }
 }
