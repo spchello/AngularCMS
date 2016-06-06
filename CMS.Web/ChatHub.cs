3c2ef0763f90e440b6546c9cf19a700d97a5b091
spj <peijyun.shih@gmail.com>
2016-06-06 14:09:25 +0800

    ADD ChatHub.cs，接收前端使用者名字，和訊息，當然使用者名稱也可以抓目前登入資訊。而回傳訊息和時間給所有使用者。

---
+++ CMS.Web/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Web
+{
+    [HubName("ChatHub")]
+    public class ChatHub : Hub
+    {
+        public void send(string Name, string Message)
+        {
+            Clients.All.addMessageToPage(Name, Message, DateTime.Now.ToString("mm:dd:ss"));
+        }
+    }
+}
