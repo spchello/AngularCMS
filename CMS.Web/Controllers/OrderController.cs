069e5c3a86309eaec72cb804680b1604ea187f4f
spj <peijyun.shih@gmail.com>
2016-06-06 14:35:53 +0800

    ADD  OrderController 取得 OrderSevice.GetCustomerOrders() 的資料

---
+++ CMS.Web/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using CMS.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            var models = OrderSevice.GetCustomerOrders();
+            return View(models);
+        }
+    }
+}
