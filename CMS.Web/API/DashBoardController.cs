9aeabd01253275f332427a777f8dd2332ccb2096
shih <peijyun.shih@gmail.com>
2016-06-06 13:43:01 +0800

    ADD  Web API新增DashBoardController, Web API就很單純的去跟BLL層要資料並回傳

---
+++ CMS.Web/API/DashBoardController.cs
@@ -0,0 +1,35 @@
+﻿using CMS.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CMS.Web.API
+{
+    public class DashBoardController : ApiController
+    {
+        private DashBoardService service;
+
+        public DashBoardController()
+        {
+            service = new DashBoardService(); //BLL層
+        }
+
+
+        //向BLL層要資料
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var datas = service.GetEmpOrders();
+                return Request.CreateResponse(HttpStatusCode.OK, datas);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message.ToString());
+            }
+        }
+    }
+}
