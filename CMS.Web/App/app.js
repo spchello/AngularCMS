dd677137f6276c47eba254c26160978149e9fd4f
shih <peijyun.shih@gmail.com>
2016-06-06 10:57:42 +0800

    ADD 設定路由

--- CMS.Web/App/app.js
+++ CMS.Web/App/app.js
@@ -1,1 +1,24 @@
 ﻿var MainApp = angular.module('MainApp', ['ngRoute', 'ui.bootstrap']);
+
+MainApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    var viewBase = '/App/';
+
+    $routeProvider
+    //客戶列表　        
+    .when('/Customer', { // href
+        controller: 'CustCtrl', //對應的controller
+        templateUrl: viewBase + 'CustomerApp/List.html' //對應的頁面
+    })
+    //新增客戶
+    .when('/Customer/Add', {
+        controller: 'CustAddCtrl',
+        templateUrl: viewBase + 'CustomerApp/AddCustomer.html'
+    })
+    
+    $locationProvider.html5Mode(
+       {
+           enabled: true,
+           requireBase: false
+       });
+}]);
