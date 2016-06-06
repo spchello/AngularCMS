df4f8e184a466140442b93af6d3d682e85087629
shih <peijyun.shih@gmail.com>
2016-06-06 13:09:57 +0800

    ADD 新增客戶controller  CustAddCtrl

---
+++ CMS.Web/App/CustomerApp/customerAddController.js
@@ -0,0 +1,29 @@
+﻿MainApp.controller('CustAddCtrl', function ($scope, $location, $route, CustService) {
+    $scope.IsLoad = true;
+
+    $scope.CustomerID = '';
+    $scope.CompanyName = '';
+    $scope.ContactName = '';
+
+    $scope.Add = function () {
+
+        var Customer = {
+            CustomerID: $scope.CustomerID,
+            CompanyName: $scope.CompanyName,
+            ContactName: $scope.ContactName,
+        }
+
+
+        CustService.AddData(Customer).then(function () {
+            alert("add成功");
+            $scope.IsLoad = false;
+            $scope.CustomerID = '';
+            $scope.CompanyName = '';
+            $scope.ContactName = '';
+        }, function () {
+            alert("add失敗");
+            $scope.IsLoad = false;
+        });
+
+    };
+});
