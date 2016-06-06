ba07b558a90ddf506a90d86b3656e70ea5a2ed59
shih <peijyun.shih@gmail.com>
2016-06-06 13:29:30 +0800

    ADD ViewModel 所有員工擁有的訂單數

---
+++ CMS.Domain/ViewModels/EmpOrdersViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Domain.ViewModels
+{
+    class EmpOrdersViewModel
+    {
+        //員工編號PK
+        public int EmplyeeID { get; set; }
+        //員工姓名
+        public string EmplyeeName { get; set; }
+        //職稱
+        public string Title { get; set; }
+        //訂單數量
+        public int OrderCount { get; set; }
+    }
+}
