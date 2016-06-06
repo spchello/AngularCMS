9a4d8883e5cc6bde68851979887aa1b8d22fcc34
spj <peijyun.shih@gmail.com>
2016-06-06 14:33:32 +0800

    ADD BLL層建立OrderSevice 回傳 List<sp_Customer_Orders_Result>

---
+++ CMS.BLL/Services/OrderSevice.cs
@@ -0,0 +1,20 @@
+﻿using CMS.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.BLL.Services
+{
+    public class OrderSevice
+    {
+        public static List<sp_Customer_Orders_Result> GetCustomerOrders()
+        {
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+                return db.sp_Customer_Orders().ToList();
+            }
+        }
+    }
+}
